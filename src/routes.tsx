f6a37428cec78bea85f7db82e0cce06ae142978a
Rafael-H-Gomes <rafaelhgomes15@gmail.com>
2020-08-04 00:56:27 -0300

    Spa Activated

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,2 +1,18 @@
 import React from 'react';
-import {} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import TeachersList from './pages/TeacherList';
+import TeacherForm from './pages/TeacherForm';
+
+function Routes() {
+    return(
+        <BrowserRouter>
+            <Route path="/" exact component={Landing} />
+            <Route path="/study" exact component={TeachersList} />
+            <Route path="/give-classes" exact component={TeacherForm} />
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
