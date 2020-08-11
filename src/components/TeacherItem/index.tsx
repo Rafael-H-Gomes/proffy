88cc2a9a1c84348bbee766e37bd811e6db1e4083
Rafael-H-Gomes <rafaelhgomes15@gmail.com>
2020-08-10 22:04:58 -0300

    teacher list finalized

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
 
-import './styles.css'
-
 import WhatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem() {
+import './styles.css'
+
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/68865878?s=60&v=4" alt="Rafael Gomes"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Rafael Gomes</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
                 </header>
-                <p>
-                    Entusiasta das melhores tecnologias de química avançada.
-                    <br/><br/>
-                    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões. 
-                </p>
+                <p>{teacher.bio}</p>
 
                 <footer>
                     <p>
                         Preço/hora
-                        <strong>R$ 80,00</strong>
+                        <strong>{teacher.cost}</strong>
                     </p>
                     <button type="button">
                         <img src={WhatsappIcon} alt="Whatsapp"/>
